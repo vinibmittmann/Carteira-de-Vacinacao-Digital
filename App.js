1abd00b86e88eb93f5c93d78f881cd287d974a69
Bernardo Facchi <cataratasdolly@outlook.com>
2022-10-13 17:47:34 -0300

    Campos de login

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        
+        <TextInput
+          style={styles.textInput}
+          placeholder = "CPF / e-mail"
+        />
+
+        <TextInput
+          style = {styles.textInput}
+          placeholder = "Senha"
+        />
+      
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +28,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    height: 36,
+    backgroundColor: "#FAFAFA",
+    marginVertical: 10,
+    includeFontPadding: true,
+    paddingLeft: 5,
+    }
 });
