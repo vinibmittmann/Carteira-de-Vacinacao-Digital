6ade1082ffa0d0fb116b77f2039fa2d782349895
Bernardo Facchi <cataratasdolly@outlook.com>
2022-10-18 13:47:49 -0300

    styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-
+import { View, TextInput } from 'react-native';
+import { styles } from './styles'
 
 export default function App() {
   return (
@@ -20,22 +20,3 @@ export default function App() {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textInput: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 300,
-    height: 36,
-    backgroundColor: "#FAFAFA",
-    marginVertical: 10,
-    includeFontPadding: true,
-    paddingLeft: 5,
-    }
-});
---
+++ styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textInput: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 300,
+      height: 36,
+      backgroundColor: "#FAFAFA",
+      marginVertical: 10,
+      includeFontPadding: true,
+      paddingLeft: 5,
+      borderRadius: 16,
+      }
+  });
+  
